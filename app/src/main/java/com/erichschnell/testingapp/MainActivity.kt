9d3c142fafb4eb1e3f4460a9ba9ad4cb92b677fe
Erich Schnell <erichschnelldev@gmail.com>
2026-03-13 23:15:02 -0300

    Agregamos navegacion

--- app/src/main/java/com/erichschnell/testingapp/MainActivity.kt
+++ app/src/main/java/com/erichschnell/testingapp/MainActivity.kt
@@ -11,20 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.erichschnell.testingapp.presentation.navigation.NavGraph
 import com.erichschnell.testingapp.ui.theme.TestingAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TestingAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()
             }
         }
     }
---
+++ app/src/main/java/com/erichschnell/testingapp/TestingApp.kt
@@ -0,0 +1,7 @@
+package com.erichschnell.testingapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TestingApp: Application()
