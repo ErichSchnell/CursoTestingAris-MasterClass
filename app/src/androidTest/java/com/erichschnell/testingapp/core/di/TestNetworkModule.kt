ba99a06d6e8c5bf8c4526309a041060574b5bc2c
Erich Schnell <erichschnelldev@gmail.com>
2026-04-22 12:01:49 -0300

    Generamos TestNetworkModule para remplazar NetworkModule en los test

---
+++ app/src/androidTest/java/com/erichschnell/testingapp/core/di/TestNetworkModule.kt
@@ -0,0 +1,61 @@
+package com.erichschnell.testingapp.core.di
+
+import androidx.test.espresso.core.internal.deps.dagger.Module
+import com.erichschnell.testingapp.core.mockwebserver.MockWebServerUrlHolder
+import com.erichschnell.testingapp.data.remote.MiniMarketApiService
+import com.erichschnell.testingapp.di.NetworkModule
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [NetworkModule::class]
+)
+object TestNetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json {
+        return Json {
+            ignoreUnknownKeys = true
+            isLenient = true
+            coerceInputValues = true
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json,
+    ): Retrofit {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(MockWebServerUrlHolder.baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideMiniMarketApiService(retrofit: Retrofit): MiniMarketApiService {
+        return retrofit.create(MiniMarketApiService::class.java)
+    }
+
+}
